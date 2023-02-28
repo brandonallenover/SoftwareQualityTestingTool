fe6c3ebc62a0417613cb4b30dfd48b4292fe22a5
brandonallenover <99698479+brandonallenover@users.noreply.github.com>
2023-02-28 15:33:35 +1100

    initial commit from brandon testing intellij git integration

---
+++ src/packages/DepthOfConditionalNesting.java
@@ -0,0 +1,10 @@
+package packages;
+
+class DepthOfConditionalNesting {
+    public DepthOfConditionalNesting(){
+
+    }
+    public void run() {
+        System.out.println("test depth of conditional nesting");
+    }
+}
